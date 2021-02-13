836bca1d31f523f35839a464c36064df7d878a89
Ahmaud R. Templeton <ahmaud@gmail.com>
2021-02-13 17:44:11 -0500

    commit tree eb3c740da583129ee67d92838061f7ba74c36d05

--- lib/customer.rb
+++ lib/customer.rb
@@ -1,3 +1,32 @@
+require 'pry'
+require_relative './meal.rb'
+require_relative './waiter.rb'
+
 class Customer
-  
+    attr_accessor :name, :age
+
+    @@all = []
+
+    def initialize(name, age)
+        @name = name
+        @age = age
+        @@all << self
+    end
+
+    def self.all
+        @@all
+    end
+
+    def new_meal(waiter, total, tip)
+        Meal.new(waiter, self, total, tip)
+    end
+
+    def meals
+        Meal.all.select { |order| order.customer == self }
+    end
+
+    def waiters
+        meals.map { |order| order.waiter }
+    end
+
 end
--- lib/waiter.rb
+++ lib/waiter.rb
@@ -1,3 +1,44 @@
-class Waiter
+require 'pry'
+require_relative './customer.rb'
+require_relative './meal.rb'
 
+class Waiter
+    attr_accessor :name, :years
+
+    @@all = []
+
+    def initialize(name, years)
+        @name = name
+        @years = years
+        @@all << self
+    end
+
+    def self.all
+        @@all
+    end
+
+    def new_meal(customer, total, tip)
+        Meal.new(self, customer, total, tip)
+    end
+
+    def meals
+        Meal.all.select { |order| order.waiter == self }
+    end
+
+    def best_tipper
+        customer_tips = nil
+        big_tip = meals.map { |customer| customer.tip }
+        # tips = meals.map do |customer|
+        #     customer.tip.max
+        # end
+        # puts tips.max
+        big_tipper = big_tip.max
+        binding.pry
+        meals.select do |customer| 
+            if customer.tip == big_tipper
+                customer_tips = customer.customer
+            end
+        end
+        customer_tips
+    end
 end
